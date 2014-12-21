8f8a0310c02dfbbaf259a7ef457c17ca1bb5b010
Arnaud de Villeneuve <arnaudv@mac.com>
2014-12-21 20:17:18 +0100

    First draft of a proof-of-concept
    Proof-of-concept of the communication between 2 workstations using
    signals.

---
+++ source/atelier1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int NbInput = 20; // Nombre de ressources en stock pour la production
+int NbOutput = 0; // Nombre de produits en stock prets a etre expedies
+
+void verifierInputStock(){
+
+}
+
+void produire(){
+
+}
+
+
+
+
+void atelier(){
+
+}
---
+++ source/main.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define NbAtelier1 1      //Nombre de processus symbolisant les ateliers 1
+#define NbAtelier2 1      //Nombre de processus symbolisant les ateliers 2
+#define NbAtelier3 1      //Nombre de processus symbolisant les ateliers 3
+
+pthread_t tid[NbAtelier1+NbAtelier2+NbAtelier3+1];
+pthread_mutex_t mutex;
+pthread_cond_t atelier1_2, atelier2_3;
+
+/* Initialisation */
+
+// Ressources generiques
+int NbInput = 50;		// Nombre de ressources du stock originel
+int NbOutput = 0;		// Nombre de produits finis
+
+// Atelier 1
+int NbInput1 = 20;	// Nombre de ressources en stock pour la production de l'atelier 1
+int NbOutput1 = 0;	// Nombre de produits en stock prets a etre expedies de l'atelier 1
+
+// Atelier 2
+int NbInput2 = 20;	// Nombre de ressources en stock pour la production de l'atelier 2
+int NbOutput2 = 0;	// Nombre de produits en stock prets a etre expedies de l'atelier 2
+
+// Atelier 3
+int NbInput3 = 20;	// Nombre de ressources en stock pour la production de l'atelier 3
+int NbOutput3 = 0;	// Nombre de produits en stock prets a etre expedies de l'atelier 3
+
+
+
+void atelier2(int i){
+
+pthread_mutex_lock(&mutex);
+	while ( NbInput2 > 0 ){
+		pthread_mutex_unlock(&mutex);
+		
+		/* Verification stock input */
+		pthread_mutex_lock(&mutex);
+		if ( NbInput2 == 0 ){
+			printf("L'atelier %d de niveau 2 a epuise son stock.\n", (int) i);
+		}
+		pthread_mutex_unlock(&mutex);
+		
+		
+		/* Production */
+		pthread_mutex_lock(&mutex);
+		if ( NbInput2 < 10 ){
+			printf("L'atelier %d de niveau 2 produit.\n", (int) i);
+			//sleep(1);
+			NbInput2--;
+			NbOutput2++;
+		}
+		pthread_mutex_unlock(&mutex);
+		
+		
+		/* Expedition */
+		pthread_mutex_lock(&mutex);
+		if ( NbOutput2 >= 10 ){
+			pthread_cond_wait(&atelier1_2, &mutex);
+      NbOutput2 -= 10;
+      NbInput1 += 10;
+		}
+		pthread_mutex_unlock(&mutex);
+	}
+  pthread_mutex_unlock(&mutex);
+}
+
+
+
+void atelier1(int i) {
+
+pthread_mutex_lock(&mutex);
+	while ( NbInput1 > 0 ){
+		pthread_mutex_unlock(&mutex);
+
+
+	/* Verification stock input */
+	pthread_mutex_lock(&mutex);
+	if ( NbInput1 < 11 ){
+		pthread_cond_signal(&atelier1_2);
+	}
+	pthread_mutex_unlock(&mutex);
+
+	/* Production */
+	pthread_mutex_lock(&mutex);
+	//sleep(2);
+	NbInput1--;
+	NbOutput1++;
+	pthread_mutex_unlock(&mutex);
+	printf("L'atelier %d de niveau 1 produit. Input: %d Output: %d\n", (int) i, NbInput1, NbOutput1);
+  
+  /* Expedition */
+  pthread_mutex_lock(&mutex);
+  	if ( NbOutput1 >= 10 ){
+    	printf("L'atelier %d de niveau 1 expedie un container.\n", (int) i);
+      NbOutput1 -= 10;
+      NbOutput += 10;
+  }
+  pthread_mutex_unlock(&mutex);
+  
+	}
+  printf("L'atelier %d de niveau 1 a epuise son stock.\n", (int) i);
+	pthread_mutex_unlock(&mutex);
+}
+
+
+
+int main()
+{
+  //creation des threads ateliers
+  pthread_create(tid,0,(void *(*)())atelier1,(void*)1);
+  pthread_create(tid+1,0,(void *(*)())atelier2,(void*)2);
+  //pthread_create(tid+2,0,(void *(*)())atelier3,(void*)3);
+
+  //attend la fin de toutes les threads voitures
+	pthread_join(tid[0],NULL);
+	pthread_join(tid[1],NULL);
+  
+  pthread_mutex_lock(&mutex);
+  printf("Quantite de produits obtenue: %d\n",NbOutput);
+  pthread_mutex_unlock(&mutex);
+  
+  /* liberation des ressources");*/
+  exit(0);
+}
